48a934a554ca54290cf6bbed60f3a65e29db6781
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-10-24 14:26:23 +0300

    file change

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
-const apiURL = "https://opentdb.com/api.php?amount=20&category=11&difficulty=easy&type=multiple"
 
-getTriviaInformation()
+const apiURL = 'http://localhost:3000/results'
 function getTriviaInformation(){
     fetch(apiURL)
     .then((response) => response.json())
     .then(data => {
         displayTriviaQuestions(data),
-        console.log(data);
+        console.log(data)
     })
 }
 function displayTriviaQuestions(){
